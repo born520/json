b907f3755f9a2c09cc4558fd6e93171b906d12cc
born520 <born520@gmail.com>
2024-08-22 14:51:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,43 +9,19 @@ function renderTable(data) {
     const container = document.getElementById('table-container');
     const table = document.createElement('table');
 
-    const tableData = data.tableData;
-    const mergedCells = data.mergedCells;
-
-    const rowspanTracker = []; // 각 열에 대해 rowspan이 진행 중인지 추적
-
-    tableData.forEach((rowData, rowIndex) => {
+    data.forEach((rowData, rowIndex) => {
         const row = document.createElement('tr');
-        let colIndexOffset = 0; // 병합된 셀로 인해 발생하는 열 인덱스 오프셋
 
         rowData.forEach((cellData, colIndex) => {
-            const actualColIndex = colIndex + colIndexOffset;
-
-            // 이미 rowspan에 의해 차지된 셀 건너뛰기
-            while (rowspanTracker[actualColIndex] > 0) {
-                rowspanTracker[actualColIndex]--;
-                colIndexOffset++;
-            }
-
-            if (cellData !== null) {
+            if (cellData) { // cellData가 유효한 경우에만 처리
                 const cell = document.createElement('td');
                 cell.textContent = cellData.text || '';
 
-                if (cellData.style) {
-                    Object.assign(cell.style, cellData.style);
+                if (cellData.rowSpan) {
+                    cell.rowSpan = cellData.rowSpan;
                 }
-
-                // 병합 정보에 따라 셀의 병합 적용
-                const mergeInfo = mergedCells.find(merge => merge.row === rowIndex && merge.column === colIndex);
-                if (mergeInfo) {
-                    if (mergeInfo.numRows > 1) {
-                        cell.rowSpan = mergeInfo.numRows;
-                        rowspanTracker[actualColIndex] = mergeInfo.numRows - 1;
-                    }
-                    if (mergeInfo.numColumns > 1) {
-                        cell.colSpan = mergeInfo.numColumns;
-                        colIndexOffset += mergeInfo.numColumns - 1;
-                    }
+                if (cellData.colSpan) {
+                    cell.colSpan = cellData.colSpan;
                 }
 
                 row.appendChild(cell);
