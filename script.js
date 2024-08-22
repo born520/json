7fc480b7fd406fe1210061f6420e73c5d6b1bf04
born520 <born520@gmail.com>
2024-08-22 15:12:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,31 @@ function renderTable(data) {
     
     data.tableData.forEach((rowData, rowIndex) => {
         const row = table.insertRow();
+
+        // 병합된 셀의 열 인덱스를 추적하기 위한 배열
         let cellIndex = 0;
-
         rowData.forEach((cellData, colIndex) => {
-            if (cellData) {
-                const cell = row.insertCell(cellIndex);
+            // 이미 병합된 셀의 경우 다음 열로 건너뜀
+            while (row.cells[cellIndex]) {
+                cellIndex++;
+            }
 
-                // 병합된 셀 처리
-                const mergedCell = data.mergedCells ? data.mergedCells.find(mc => mc.row === rowIndex + 1 && mc.column === colIndex + 1) : null;
+            const cell = row.insertCell(cellIndex);
 
-                if (mergedCell) {
-                    cell.rowSpan = mergedCell.numRows;
-                    cell.colSpan = mergedCell.numColumns;
-                    cell.innerHTML = cellData.richText || cellData.text || '';
-                    applyStyles(cell, rowIndex, colIndex, data);
-                    
-                    // 병합된 셀 범위만큼 인덱스 증가
-                    cellIndex += mergedCell.numColumns;
-                } else {
-                    cell.innerHTML = cellData.richText || cellData.text || '';
-                    applyStyles(cell, rowIndex, colIndex, data);
-                    cellIndex++;
-                }
+            // 병합된 셀 처리
+            const mergedCell = data.mergedCells ? data.mergedCells.find(mc => mc.row === rowIndex + 1 && mc.column === colIndex + 1) : null;
+
+            if (mergedCell) {
+                cell.rowSpan = mergedCell.numRows;
+                cell.colSpan = mergedCell.numColumns;
+                cell.innerHTML = cellData.richText || cellData.text || '';
+                applyStyles(cell, rowIndex, colIndex, data);
+                
+                // 병합된 셀 범위만큼 인덱스 증가
+                cellIndex += mergedCell.numColumns;
             } else {
+                cell.innerHTML = cellData.richText || cellData.text || '';
+                applyStyles(cell, rowIndex, colIndex, data);
                 cellIndex++;
             }
         });
